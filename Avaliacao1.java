ce47a036f99f268c49df3524085a6dab099d2ced
Evinhaaa010 <evelyn.ibiapina@aluno.edu.pi.gov.br>
2025-07-08 13:20:46 -0300

    first commit

---
+++ Avaliacao1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Avaliacao1 {
+    public static void main(String[] args) {
+               
+        Scanner input=new Scanner(System.in);
+
+        float n1= input.nextFloat();
+        float n2= input.nextFloat();
+        float n3= input.nextFloat();
+        float n4= input.nextFloat();
+        float n5= input.nextFloat();
+        float n6= input.nextFloat();
+        float n7= input.nextFloat();
+        float n8= input.nextFloat();
+
+
+
+        
+
+
+
+
+
+
+
+    }
+}
---
+++ Avaliacao2.java
@@ -0,0 +1,5 @@
+public class Avaliacao2 {
+    public static void main(String[] args) {
+        
+    }
+}
